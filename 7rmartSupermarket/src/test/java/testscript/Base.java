bf3d857f98b506a1dee8db06b02b6538fd9cb6f7
anany <ananyajayshan@gmail.com>
2026-06-17 17:01:55 +0530

    updated-crossbrowser,multibrowser

--- 7rmartSupermarket/src/test/java/testscript/Base.java
+++ 7rmartSupermarket/src/test/java/testscript/Base.java
@@ -3,6 +3,7 @@ package testscript;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -11,22 +12,29 @@ import org.testng.annotations.Parameters;
 import utilities.WaitUtility;
 
 public class Base {
-	
+
 	public WebDriver driver;
-	
+
 	@BeforeMethod
-	//@Parameters("browser")
-	public void browserInitialization() {
-		driver=new EdgeDriver();
+	@Parameters("browser")
+	public void browserInitialization(String browser) throws Exception {
+		if (browser.equalsIgnoreCase("Edge")) {
+			driver = new EdgeDriver();
+		} else if (browser.equalsIgnoreCase("Chrome")) {
+			driver = new ChromeDriver();
+		} else {
+			throw new Exception("Invalid");
+		}
+		// driver=new EdgeDriver();
 		driver.get("https://groceryapp.uniqassosiates.com/admin");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WaitUtility.IMPLICITWAIT));
 		driver.manage().window().maximize();
 	}
-	
+
 	@AfterMethod
 	public void browserQuitandClose() {
 		driver.close();
-		//driver.quit();
+		// driver.quit();
 	}
 
 }
